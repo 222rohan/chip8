270760848612618e4f5c5a815c5516168bbfbf62
222rohan <rohanshenoy405@gmail.com>
2025-04-08 16:31:24 +0530

    add sdl events

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <unistd.h>
 #include "chip8.h"
 
 #include<SDL2/SDL.h>
@@ -18,18 +19,124 @@ using namespace std;
 #define MODE_VRB 000000001
 #define MODE_SND 000000010
 
-CHIP8 chip8_instance;
+/* State of the machine, will be used for trace, and running. */
+enum MACHINESTATE {EMU_ON, EMU_READY, EMU_RUN, EMU_STOP, EMU_OFF, EMU_UNDEF};
+MACHINESTATE STATE = EMU_OFF; 
 
-// enum MACHINESTATE {ON, READY, RUN, STOP, OFF, UNDEF};
+/* SDL2 paramters, window width and height. */
+#define WIN_WD 640
+#define WIN_HT 320
+uint8_t keymap[16] = {
+/*
+    this is the keypad layout of CHIP8            mapped to keyboard
+            1	2	3	C                           1	2	3	4
+            4	5	6	D               ----->      Q	W	E	R
+            7	8	9	E                           A	S	D	F
+            A	0	B	F                           Z	X	C	V               
+*/
+    SDLK_x, SDLK_1, SDLK_2, SDLK_3,
+    SDLK_q, SDLK_w, SDLK_e, SDLK_a,
+    SDLK_s, SDLK_d, SDLK_z, SDLK_c,
+    SDLK_4, SDLK_r, SDLK_f, SDLK_v,
+};
+
+void    parse_commands(int, char*[], uint8_t*);
+int     setup_rom(CHIP8*, char*, uint8_t);
+int     setup_window();
+int     exit_emu(int );
+
+int main(int argc, char *argv[]) {
+    STATE = EMU_ON;
+    /*
+        A byte which determines the various modes (see options)
+
+        Default                                 (00000000).
+        Verbose ON : right-most bit     ON      (00000001).
+        Sound   OFF: 2nd from right bit ON.     (00000010).
+    */
+    uint8_t MODE = 0;
+
+    parse_commands(argc,argv, &MODE);
+
+    CHIP8 chip8_instance;
+    
+    if(setup_rom(&chip8_instance, argv[1], MODE) == -1) {
+        cerr<<std::endl<<"could not open ROM file.";
+        exit(1);
+    }
+
+    if(setup_window() == -1) {
+        cerr<<std::endl<<"could not setup SDL2 window.";
+        exit(1);
+    }
+
+    /*
+        SETUP WINDOW using SDL2
+        SDL window, Pointers
+        Reference: https://thenumb.at/cpp-course/index.html#sdl
+    */
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer;
+
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 ) {
+        cerr << "Error initializing SDL: " << SDL_GetError() << endl;
+        system("pause");
+        return -1;
+    } 
+
+    int result = SDL_CreateWindowAndRenderer( WIN_WD, WIN_HT, 0, &window, &renderer );
+    if ( result != 0 ) {
+        cout << "Failed to create window and renderer: " << SDL_GetError() << endl;
+    }
+    SDL_RenderSetLogicalSize(renderer, WIN_WD, WIN_HT);
+
+    while(STATE == EMU_ON){
+        chip8_instance.cycle();
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT){
+                STATE = EMU_OFF;
+            }
+
+            if(event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    STATE = EMU_OFF;
+                }
+
+                for (int i = 0; i < MAX_KEYCOUNT; i++)
+                {
+                    if (event.key.keysym.sym == keymap[i])
+                    {
+                        chip8_instance.set_key(i, KEY_DOWN);
+                    }
+                }                
+            }
+
+            if(event.type == SDL_KEYUP) {
+                for (int i = 0; i < MAX_KEYCOUNT; i++)
+                {
+                    if (event.key.keysym.sym == keymap[i])
+                    {
+                        chip8_instance.set_key(i, KEY_UP);
+                    }
+                } 
+            }
+        }
+        sleep(1);
+    }
+
+    return 0;
+}
 
 void parse_commands(int argc, char* argv[], uint8_t *MODE){
     if(argc < 2) {
-        cout<<"usage: ./chip8 <rom>.ch8 <-options[hvac]>";
+        cout<<"usage: ./chip8 <rom>.ch8 <-options[hvac]>"<<endl;
         exit(0);
     }
 
     if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "help") == 0) {
-        cout<<"usage: ./chip8 <rom>.ch8 <options>"<<endl;
+        cout<<"usage: ./chip8 <rom>.ch8 <-options[hvac]>"<<endl;
         cout<<"options:"<<endl;
         cout<<"\t-h : shows this message."<<endl;
         cout<<"\t-v : verbose mode, shows interal trace."<<endl;
@@ -39,29 +146,40 @@ void parse_commands(int argc, char* argv[], uint8_t *MODE){
     }
 
     if(argc >= 3 && argv[2][0] == '-'){
+        bool option_correct = false;
         string options = argv[2];
 
         if(options.find("h") != string::npos){
-            cout<<"usage: ./chip8 <rom>.ch8 <options>"<<endl;
+            cout<<"usage: ./chip8 <rom>.ch8 <-options[hvac]>"<<endl;
             cout<<"options:"<<endl;
             cout<<"\t-h : shows this message."<<endl;
             cout<<"\t-v : verbose mode, shows interal trace."<<endl;
             cout<<"\t-a : disables audio."<<endl;
             cout<<"\t-c : displays controls."<<endl;
+            cout<<endl;
+            option_correct = true;
         }
 
         if(options.find("v") != string::npos) {
             cout<<"VERBOSE(trace) mode is ON."<<endl;
             *MODE |= MODE_VRB;
+            option_correct = true;
         }
     
         if(options.find("a") != string::npos) {
             cout<<"AUDIO is OFF."<<endl;
             *MODE |= MODE_SND;
+            option_correct = true;
         }
     
         if(options.find("c") != string::npos) {
             cout << "CONTROLS:";
+            option_correct = true;
+        }
+
+        if(!option_correct) {
+            cout << "invalid option. check valid options using ./chip -h"<<endl;
+            exit(1);
         }
     }
     
@@ -69,7 +187,7 @@ void parse_commands(int argc, char* argv[], uint8_t *MODE){
 
 }
 
-int setup_rom(char *rom, uint8_t MODE) {
+int setup_rom(CHIP8 *chip8_instance, char *rom, uint8_t MODE) {
     //for now call load_rom directly, add fancy path checkers later
     bool sound = false;
     bool verbose = false;
@@ -81,76 +199,13 @@ int setup_rom(char *rom, uint8_t MODE) {
         verbose = true;
     }
 
-    return chip8_instance.load_rom(rom, sound, verbose);
+    return chip8_instance->load_rom(rom, sound, verbose);
 }
 
-int main(int argc, char *argv[]) {
-
-    /*
-        A byte which determines the various modes (see options)
-
-        Default                                 (00000000).
-        Verbose ON : right-most bit     ON      (00000001).
-        Sound   OFF: 2nd from right bit ON.     (00000010).
-    */
-    uint8_t MODE = 0;
-
-    parse_commands(argc,argv, &MODE);
-    
-    if(setup_rom(argv[1], MODE) == -1) {
-        cerr<<std::endl<<"could not open ROM file.";
-        exit(1);
-    }
-
-    /*
-        SETUP WINDOW        
-        SDL window,surface Pointers
-        Code from https://thenumb.at/cpp-course/sdl2/01/01.html
-    */
-    SDL_Surface* winSurface = NULL;
-    SDL_Window* window = NULL;
-
-	// Initialize SDL. SDL_Init will return -1 if it fails.
-	if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 ) {
-		cerr << "Error initializing SDL: " << SDL_GetError() << endl;
-		system("pause");
-		return 1;
-	} 
-
-	// Create  window
-	window = SDL_CreateWindow( "Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
-
-	// check window
-	if ( !window ) {
-		cout << "Error creating window: " << SDL_GetError()  << endl;
-		system("pause");
-		return 1;
-	}
-
-	// Get the surface from the window
-	winSurface = SDL_GetWindowSurface( window );
-
-	// check surface
-	if ( !winSurface ) {
-		cout << "Error getting surface: " << SDL_GetError() << endl;
-		system("pause");
-		return 1;
-	}
-
-    SDL_FillRect( winSurface, NULL, SDL_MapRGB( winSurface->format, 255, 255, 255 ) );
-
-	// Update the window display
-	SDL_UpdateWindowSurface( window );
-
-	// Wait
-	system("pause");
-
-	// Destroy the window. This will also destroy the surface
-	SDL_DestroyWindow( window );
-
-	// Quit SDL
-	SDL_Quit();
-
-
+int setup_window() {
     return 0;
+}
+
+int exit_emu(int code) {
+    exit(code);
 }
